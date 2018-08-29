2f599cdac31cca1e40ac3851e9e6b797dab55181
YanC Lin <yonalin1@gmail.com>
2018-08-29 19:42:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function BoardMember(name,homeState,training){
     return("I like to help people.")
   }
   this.releasePressStatement = function(){
-    return("You will see great things from Scruber.")
+    return("You will see great things from Scuber.")
   }
   this.sayHi = function(){
     return("Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.")
