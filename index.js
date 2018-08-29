68089eebb66a301aecc0aa1f0858e50e0091c6f0
YanC Lin <yonalin1@gmail.com>
2018-08-29 19:37:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ function BoardMember(name,homeState,training){
   this.veto = function(){
     return ("No, I must disagree");
   }
+  this.approve = function(){
+    return ("You can do that!")
+  }
 }
