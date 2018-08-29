a917c52e33f323a49b0c0a2f3aaf2f66c98e9f8a
YanC Lin <yonalin1@gmail.com>
2018-08-29 19:30:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ function BoardMember(name,homeState,training){
   this.name = name
   this.homeState = homeState
   this.training = training
+  this.veto = function(){
+    console.log('No,I must disagree')
+}
 }
