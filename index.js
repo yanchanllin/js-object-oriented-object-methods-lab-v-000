ce866b1f987ea9134580c77eec831604ed05983d
YanC Lin <yonalin1@gmail.com>
2018-08-29 19:38:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,7 @@ function BoardMember(name,homeState,training){
   this.approve = function(){
     return ("You can do that!")
   }
+  this.doCharity = function(){
+    return("I like to help people")
+  }
 }
