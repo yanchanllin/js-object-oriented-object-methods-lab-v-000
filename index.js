5cbc688627fe3748a6e4ab39a1f21015f8c0994c
YanC Lin <yonalin1@gmail.com>
2018-08-29 19:44:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ function BoardMember(name,homeState,training){
     return ("No, I must disagree");
   }
   this.approve = function(){
-    return ("You can do that!")
+    return ("You can do that!");
   }
   this.doCharity = function(){
-    return("I like to help people.")
+    return("I like to help people.");
   }
   this.releasePressStatement = function(){
-    return("You will see great things from Scuber.")
+    return("You will see great things from Scuber.");
   }
   this.sayHi = function(){
-    return("Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.")
+    return("Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.");
   }
 }
